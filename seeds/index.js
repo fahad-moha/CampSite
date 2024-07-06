09663c8b31aa16dbe87fcd27120f0619817bdd0d
fahad-moha <khiladifahad02@gmail.com>
2024-07-06 14:19:04 +0300

    adding basic styles

--- seeds/index.js
+++ seeds/index.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const axios = require('axios');
+
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
@@ -18,19 +20,47 @@ db.once("open", () => {
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
-
-const seedDB = async () => {
-    await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
-        const random1000 = Math.floor(Math.random() * 1000);
-        const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
-        })
-        await camp.save();
+  // call unsplash and return small image
+  async function seedImg() {
+    try {
+      const resp = await axios.get('https://api.unsplash.com/photos/random', {
+        params: {
+          client_id: 'pWuHbd_xDvdG5mJfb7MBT8K_7wsnv2iNp8USUxZycls',
+          //collections: 1114848,
+        },
+      })
+      //console.log("resp ", resp)
+      //console.log("resp ", resp.data)
+      return resp.data.urls.small
+    } catch (err) {
+      console.error(err)
     }
-}
+  }
 
-seedDB().then(() => {
+
+  const seedDB = async () => {
+    await Campground.deleteMany({})
+    for (let i = 0; i < 2; i++) {
+      // setup
+      const placeSeed = Math.floor(Math.random() * places.length)
+      const descriptorsSeed = Math.floor(Math.random() * descriptors.length)
+      const citySeed = Math.floor(Math.random() * cities.length)
+   
+      // seed data into campground
+      const camp = new Campground({
+        image: await seedImg(),
+        title: `${descriptors[descriptorsSeed]} ${places[placeSeed]}`,
+        location: `${cities[citySeed].city}, ${cities[citySeed].state}`,
+        description:
+          'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis, nihil tempora vel aspernatur quod aliquam illum! Iste impedit odio esse neque veniam molestiae eligendi commodi minus, beatae accusantium, doloribus quo!',
+      })
+   
+      await camp.save()
+    }
+  }
+
+  seedDB().then(() => {
     mongoose.connection.close();
 })
+  
+
